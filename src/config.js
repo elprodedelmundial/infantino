f2b4643c8ace9e8e0fbf9bdfcf78197f3e73dbf6
Cristian Raña <cristian.rana8@gmail.com>
2026-05-20 14:35:26 -0300

    feature flag added

--- src/config.js
+++ src/config.js
@@ -6,4 +6,9 @@
 window.__APP_CONFIG__ = {
   // Base URL for the Grondona API
   'grondona-url': 'https://grondona-897932846991.southamerica-east1.run.app',
+  // Firebase Web app config used for Remote Config feature flags
+  'firebase-project-id': 'elprodedelmundial-7d579',
+  'firebase-api-key': 'AIzaSyDfwFjOMJlk1sSe6jPRmpRTLheg-VEz-w8',
+  'firebase-app-id': '1:65537265534:web:16b670ef7be010038fea2f',
+  'firebase-messaging-sender-id': '65537265534',
 };
